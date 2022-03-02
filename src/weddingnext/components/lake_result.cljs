2cec57b5fef971c6ec40758194347ff1f4129d84
Benj <Benjamin.Schwerdtner@gmail.com>
2022-03-02 10:09:05 +0100

    Update

--- src/weddingnext/components/lake_result.cljs
+++ src/weddingnext/components/lake_result.cljs
@@ -3,10 +3,18 @@
     (:require
      [re-frame.core :as rf]
      [weddingnext.specs :as ws]
+     [weddingnext.components.elements :as elms]
      [weddingnext.components.input-field-with-btn
       :refer
       [input-field-and-btn]]))
 
 
 (defn lake-result []
-  [:p "Die Antwort ist"])
+  [:p "Die Antwort ist"]
+  [:div.w-30.p-4.m-2
+   [:p
+    "Die Antwort ist "]
+   [:p.text-xl.
+    "29"]
+
+   [elms/devider]])
